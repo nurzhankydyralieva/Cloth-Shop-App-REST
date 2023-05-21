f7ec22a6b74c75f2b3651fc3805b3b9752c3e74d
Nurzhan <nurzhankydyralieva@gmail.com>
2023-05-21 23:50:09 +0600

    first commit

---
+++ src/main/java/com/epam/cloth_shop_rest_app/ClothShopAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.epam.cloth_shop_rest_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothShopAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothShopAppRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/cloth_shop_rest_app/ClothShopAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.cloth_shop_rest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothShopAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
